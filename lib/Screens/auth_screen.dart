50fbeae0a4cbc8a92845df880086cdd796011fac
VaneSkubic <vane.skubic@gmail.com>
2021-03-25 21:57:58 +0100

    Login UI remake

--- lib/Screens/auth_screen.dart
+++ lib/Screens/auth_screen.dart
@@ -22,10 +22,7 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
-    // transformConfig.translate(-10.0);
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       body: GestureDetector(
         onTap: () {
           _emailFocus.unfocus();
@@ -33,48 +30,28 @@ class AuthScreen extends StatelessWidget {
           _nameFocus.unfocus();
           _surnameFocus.unfocus();
         },
-        child: Stack(
-          children: <Widget>[
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color.fromRGBO(96, 238, 255, 1).withOpacity(0.5),
-                    Color.fromRGBO(1, 98, 255, 1).withOpacity(0.9),
-                  ],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [0, 1],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 10.0, top: 60),
+                  padding:
+                      EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0),
+                  child: Image(
+                    image: AssetImage('Assets/Logo.png'),
+                  ),
                 ),
               ),
-            ),
-            SingleChildScrollView(
-              child: Container(
-                height: deviceSize.height,
-                width: deviceSize.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Flexible(
-                      child: Container(
-                        margin: EdgeInsets.only(bottom: 10.0, top: 60),
-                        padding: EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 50.0),
-                        child: Image(
-                          image: AssetImage('Assets/Logo.png'),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      flex: deviceSize.width > 600 ? 2 : 2,
-                      child: AuthCard(),
-                    ),
-                  ],
-                ),
+              Flexible(
+                flex: deviceSize.width > 600 ? 2 : 2,
+                child: AuthCard(),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -129,14 +106,12 @@ class _AuthCardState extends State<AuthCard> {
     });
     try {
       if (_authMode == AuthMode.Login) {
-        // Log user in
         await Provider.of<Auth>(context, listen: false).login(
           _authData['Email'],
           _authData['Password'],
         );
       } else {
         _switchAuthMode();
-        // Sign user up
         await Provider.of<Auth>(context, listen: false).signup(
           _authData['Name'],
           _authData['Surname'],
@@ -156,7 +131,6 @@ class _AuthCardState extends State<AuthCard> {
         errorMessage = 'Invalid eMail';
       } else if (error.toString().contains('INVALID_PASSWORD')) {
         errorMessage = 'Invalid password';
-        //_passwordController.clear();
       } else {
         errorMessage = error.toString();
       }
@@ -182,7 +156,7 @@ class _AuthCardState extends State<AuthCard> {
     _nameController.clear();
     _surnameController.clear();
 
-    _authMode = segmentedControlValue == 0 ? AuthMode.Login : AuthMode.Signup;
+    _authMode = _authMode == AuthMode.Signup ? AuthMode.Login : AuthMode.Signup;
   }
 
   @override
@@ -190,172 +164,202 @@ class _AuthCardState extends State<AuthCard> {
     final deviceSize = MediaQuery.of(context).size;
     return Column(
       children: [
-        Container(
-          width: deviceSize.width * 0.75,
-          margin: EdgeInsets.only(
-            left: 0,
-            top: 15,
-            right: 0,
-            bottom: 10,
-          ),
-          padding: EdgeInsets.all(16),
-          child: CupertinoSlidingSegmentedControl(
-              groupValue: segmentedControlValue,
-              backgroundColor: Color.fromRGBO(255, 255, 255, 1).withOpacity(.5),
-              children: <int, Widget>{
-                0: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    segmentedControlValue == 0 ? 'LOG IN' : 'Log In',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: segmentedControlValue == 0
-                          ? FontWeight.w600
-                          : FontWeight.normal,
-                      letterSpacing: 1.6,
-                    ),
-                  ),
-                ),
-                1: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    segmentedControlValue == 1 ? 'REGISTER' : 'Register',
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: segmentedControlValue == 1
-                          ? FontWeight.w600
-                          : FontWeight.normal,
-                      letterSpacing: 1.6,
-                    ),
-                  ),
-                ),
-              },
-              onValueChanged: (value) {
-                setState(() {
-                  segmentedControlValue = value;
-                  _switchAuthMode();
-                });
-              }),
-        ),
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          elevation: 20.0,
-          child: Container(
-            height: _authMode == AuthMode.Signup ? 290 : 175,
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Offstage(
-                      offstage: _authMode == AuthMode.Login ? true : false,
-                      child: TextFormField(
-                        controller: _nameController,
-                        focusNode: _nameFocus,
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                          prefixIcon: !_nameFocus.hasPrimaryFocus
-                              ? Icon(Icons.account_circle_outlined)
-                              : null,
+        Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 30,
+              right: 30,
+              top: _authMode == AuthMode.Login ? 30 : 0,
+            ),
+            child: Column(
+              children: <Widget>[
+                Offstage(
+                  offstage: _authMode == AuthMode.Login ? true : false,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Name',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
                         ),
-                        keyboardType: TextInputType.name,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Provide a name!';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          _authData['Name'] = value.trim();
-                        },
-                      ),
-                    ),
-                    Offstage(
-                      offstage: _authMode == AuthMode.Login ? true : false,
-                      child: TextFormField(
-                        controller: _surnameController,
-                        focusNode: _surnameFocus,
-                        enabled: _authMode == AuthMode.Login ? false : true,
-                        decoration: InputDecoration(
-                          labelText: 'Surname',
-                          prefixIcon: !_surnameFocus.hasPrimaryFocus
-                              ? Icon(Icons.text_fields_outlined)
-                              : null,
+                        SizedBox(
+                          height: 10,
                         ),
-                        keyboardType: TextInputType.name,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Provide a surname!';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          _authData['Surname'] = value.trim();
-                        },
-                      ),
-                    ),
-                    TextFormField(
-                      controller: _emailController,
-                      focusNode: _emailFocus,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: !_emailFocus.hasPrimaryFocus
-                            ? Icon(Icons.email_outlined)
-                            : null,
-                      ),
-                      keyboardType: TextInputType.name,
-                      validator: (value) {
-                        if (value.isEmpty || !value.contains('@')) {
-                          return 'Invalid eMail!';
-                        }
-                        return null;
-                      },
-                      onChanged: (value) {
-                        _authData['Email'] = value.trim();
-                      },
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      focusNode: _passwordFocus,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        prefixIcon: !_passwordFocus.hasPrimaryFocus
-                            ? Icon(Icons.lock_outline)
-                            : null,
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            Icons.remove_red_eye_outlined,
-                            color: Colors.grey,
-                            size: 20,
+                        TextFormField(
+                          cursorColor: Colors.black,
+                          controller: _nameController,
+                          focusNode: _nameFocus,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            fillColor: Color(0xfff0f0f0),
+                            filled: true,
                           ),
-                          onPressed: () {
-                            // Update the state i.e. toogle the state of passwordVisible variable
-                            setState(() {
-                              _passwordVisible = !_passwordVisible;
-                            });
-                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Offstage(
+                  offstage: _authMode == AuthMode.Login ? true : false,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Surname',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          cursorColor: Colors.black,
+                          controller: _surnameController,
+                          focusNode: _surnameFocus,
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            border: UnderlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            fillColor: Color(0xfff0f0f0),
+                            filled: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        cursorColor: Colors.black,
+                        controller: _emailController,
+                        focusNode: _emailFocus,
+                        obscureText: false,
+                        keyboardType: TextInputType.name,
+                        validator: (value) {
+                          if (value.isEmpty || !value.contains('@')) {
+                            return 'Invalid eMail!';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _authData['Email'] = value.trim();
+                        },
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          fillColor: Color(0xfff0f0f0),
+                          filled: true,
                         ),
                       ),
-                      obscureText: !_passwordVisible,
-                      onChanged: (value) {
-                        _authData['Password'] = value;
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Password',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        cursorColor: Colors.black,
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          fillColor: Color(0xfff0f0f0),
+                          filled: true,
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              Icons.remove_red_eye_outlined,
+                              color: Colors.grey[800],
+                              size: 20,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _passwordVisible = !_passwordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        controller: _passwordController,
+                        focusNode: _passwordFocus,
+                        obscureText: !_passwordVisible,
+                        keyboardType: TextInputType.name,
+                        onChanged: (value) {
+                          _authData['Password'] = value;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _authMode == AuthMode.Login
+                    ? 'New to BandWith?'
+                    : 'Already have an account?',
+                textAlign: TextAlign.center,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    _switchAuthMode();
+                  });
+                },
+                child: Text(
+                  _authMode == AuthMode.Login ? 'Sign Up!' : 'Log In!',
+                  textAlign: TextAlign.center,
                 ),
               ),
-            ),
+            ],
           ),
         ),
         Container(
           width: deviceSize.width * 0.55,
           margin: _authMode == AuthMode.Login
-              ? EdgeInsets.only(top: 80)
-              : EdgeInsets.only(top: 20),
+              ? EdgeInsets.only(top: 80, bottom: 40)
+              : EdgeInsets.only(top: 20, bottom: 30),
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
               elevation: 10,
@@ -363,8 +367,8 @@ class _AuthCardState extends State<AuthCard> {
                 borderRadius: BorderRadius.circular(40),
               ),
               padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-              primary: Colors.white,
-              onPrimary: Colors.black,
+              primary: Colors.black,
+              onPrimary: Colors.white,
             ),
             child: Padding(
               padding: const EdgeInsets.all(10.0),
@@ -378,7 +382,6 @@ class _AuthCardState extends State<AuthCard> {
                       ),
                     )
                   : Container(
-                      //padding: const EdgeInsets.only(top: 80.0),
                       child: CircularProgressIndicator(
                         backgroundColor: Colors.white.withOpacity(0),
                         valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -6,15 +6,7 @@ class SplashScreen extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromRGBO(96, 238, 255, 1).withOpacity(0.5),
-              Color.fromRGBO(1, 98, 255, 1).withOpacity(0.9),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            stops: [0, 1],
-          ),
+          color: Colors.white,
         ),
         child: Center(
           child: CircularProgressIndicator(),
